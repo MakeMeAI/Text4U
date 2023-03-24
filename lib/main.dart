3629895a6cd20b6fc03bcacfa345dd105a9870f5
RavenLLevitt <ravenlukelevitt@gmail.com>
2023-03-24 17:00:38 -0400

    Fixed merge bugs

--- lib/main.dart
+++ lib/main.dart
@@ -39,10 +39,10 @@ class _NavState extends State<Nav> {
   TextEditingController nameController = TextEditingController();
   PageController _pageController = PageController(initialPage: 0);
 
-  List<Widget> _pageOptions = <Widget>[
+  final List<Widget> _pageOptions = <Widget>[
     Page1(),
-    Page2(),
-    Text("Messages"),
+    const Page2(),
+    const Text("Messages"),
   ];
 
   void _onItemTap(int index) {
@@ -59,6 +59,7 @@ class _NavState extends State<Nav> {
           curve: Curves.easeInOut);
     });
   }
+
 
   // delete if fails
   @override
@@ -113,121 +114,6 @@ class _NavState extends State<Nav> {
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.black,
         items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            tooltip: 'Calls',
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            tooltip: 'Camera',
-            label: "page 2",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            tooltip: 'Chats',
-            label: "Messages",
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTap,
-      ),
-    );
-  }
-}
-/* working on --- uncomment if other fails
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-      drawer: sidebar(),
-      extendBodyBehindAppBar: true,
-      backgroundColor: Colors.transparent,
-      appBar: AppBar(
-        title: Text("Welcome back $name"),
-        backgroundColor: const Color(0x4400000000),
-        elevation: 0,
-      ),
-      body: Center (
-        child: Stack (
-          children: <Widget> [
-            Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.purple,
-                    Colors.blue,
-                  ],
-                ),
-              ),
-            ),
-            Positioned.fill(
-              child: Align(
-                alignment: Alignment.center,
-                child: _pageOptions.elementAt(_selectedIndex),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 0.0),
-              child: Align(
-                alignment: Alignment.center,
-                child: FractionalTranslation(
-                  translation: const Offset(0.0, -1.0),
-                  child: TextField(
-                    cursorColor: Colors.white,
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      labelText: "Name",
-                      hintText: "Please Enter Your Name ",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                        borderSide: const BorderSide(
-                          color: Colors.white,
-                        ),
-                      ),
-                      labelStyle: const TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    keyboardType: TextInputType.text,
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              top: MediaQuery.of(context).size.height / 2 + 50,
-              left: MediaQuery.of(context).size.width / 2 - 50,
-              child: Text("Name: " + name.toString()),
-            ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () async {
-                  // setState(() {
-                  //   name = nameController.text;
-                  // });
-                  var response = await requests.postData(nameController.text);
-                  var jsonResponse = convert.jsonDecode(response.body);
-                  setState(() {
-                    name = jsonResponse['name'];
-                  });
-                },
-
-                child: Text("Submit"),
-              ),
-            ),
-            _pageOptions[_selectedIndex],
-            //_pageOptions.elementAt(_selectedIndex),
-          ]
-        ),
-      ),
-      extendBody: true,
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0x4400000000),
-        elevation: 0,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.black,
-        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             tooltip: 'Calls',
