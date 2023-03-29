4f4a6a47123d11374284e8f48273fa0acf925dd4
alin600 <alilin.al6@gmail.com>
2023-03-29 16:19:16 -0400

    rearranged + added in the background color

--- lib/profile.dart
+++ lib/profile.dart
@@ -9,44 +9,69 @@ class _ProfileEdit extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Edit Profile'),
-      ),
-      body: Container(
-        padding: EdgeInsets.only(left: 15, top: 20, right: 15),
-        child: GestureDetector(
-          onTap:() {
-            FocusScope.of(context).unfocus();
-          },
-          child: ListView(
-            children: [
-              Center(
-                child: Stack(
-                  children: [
-                    // this container does the profile picture
-                    Container(
-                      width: 130,
-                      height: 130,
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 4, color: Colors.white),
-                        boxShadow: [
-                          BoxShadow(
-                            spreadRadius: 2,
-                            blurRadius: 10,
-                            color: Colors.black.withOpacity(0.1)
-                          )
-                        ],
-                        shape: BoxShape.circle,
-                      ),
-                    )
-
-                  ],
-                ),
-              )
-            ],
+      backgroundColor: Colors.white70.withOpacity(0.5),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.purple,
+                  Colors.blue,
+                ],
+              ),
+            ),
           ),
-        )
-
+          Container(
+            padding: EdgeInsets.only(left: 15, top: 20, right: 15),
+            child: ListView(
+              children: [
+                Center(
+                  child: Stack(
+                    children: [
+                      // this container does the profile picture
+                      Container(
+                        width: 130,
+                        height: 130,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 4, color: Colors.white),
+                          boxShadow: [
+                            BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                            ),
+                          ],
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      // position - adds the tiny edit pencil
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(width: 4, color: Colors.white),
+                            color: Colors.blue,
+                          ),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back_ios),
