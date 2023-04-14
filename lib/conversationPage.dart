b2470b6b0de935c2df853ebef4d0560dc2984765
RavenLLevitt <ravenlukelevitt@gmail.com>
2023-04-14 16:35:00 -0400

    Updated Visuals of Chat
    Fixed visual glitch when transitioning between chat list and chat. Added the gradient and recolored the text and added an X icon to close out of the chat menu.

--- lib/conversationPage.dart
+++ lib/conversationPage.dart
@@ -6,30 +6,74 @@ class ChatDisplay extends StatelessWidget {
 
   ChatDisplay({required this.chatData});
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat Display'),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: chatData.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ChatMessage(
-                    sender: chatData[index][0],
-                    messageText: chatData[index][1],
-                  );
-                },
+      backgroundColor: Colors.transparent,
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.lightBlue,
+                  Colors.blue,
+                ],
               ),
             ),
-          ],
-        ),
+            height: 80.0,
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.clear, color: Colors.black),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                SizedBox(width: 16.0),
+                Text(
+                  //REPLACE THIS WITH PERSONS NAME
+                  'Chat',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.blue,
+                    Colors.purple,
+                  ],
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: ListView.builder(
+                  itemCount: chatData.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ChatMessage(
+                      sender: chatData[index][0],
+                      messageText: chatData[index][1],
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -52,15 +96,13 @@ class ChatMessage extends StatelessWidget {
             sender,
             style: TextStyle(
               fontSize: 12.0,
-              color: Colors.grey[600],
+              color: Colors.black,
             ),
           ),
           SizedBox(height: 5.0),
           Text(
             messageText,
-            style: TextStyle(fontSize: 16.0,
-            color: Colors.white,
-            ),
+            style: TextStyle(fontSize: 16.0, color: Colors.black),
           ),
         ],
       ),
